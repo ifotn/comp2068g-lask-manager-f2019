7c9546208baf1ce233b044995db656869c7d3a7b
ifotn <rich@infrontofthenet.com>
2019-11-08 09:36:52 -0500

    live callback uri

--- config/globals.js
+++ config/globals.js
@@ -7,7 +7,7 @@ module.exports = {
         'google': {
             clientID: '771176901294-etvcdacc4vpdnls7aqn8ad6b8kshh91p.apps.googleusercontent.com',
             clientSecret: 'n5a5bcUEm7jP6SWpP_5oKgrK',
-            callbackURL: 'http://localhost:3000/google/callback'
+            callbackURL: 'https://task-manager-f19.herokuapp.com/google/callback'
         }
     }
 }
